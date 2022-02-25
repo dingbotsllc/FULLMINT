6edb36238b597ab95bb23bf5e5c3c841688cefde
dingbotsllc <98507443+dingbotsllc@users.noreply.github.com>
2022-02-24 18:51:56 -0800

    Update App.js

--- DingBots Minter/src/App.js
+++ DingBots Minter/src/App.js
@@ -240,7 +240,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY+49}
+              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
             </s.TextTitle>
             <s.TextDescription
               style={{
